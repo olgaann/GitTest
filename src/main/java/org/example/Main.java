674d0f55fffdc63f1eaa78ffa271c9abf40c71bb
olga <bochkareva-o.a@yandex.ru>
2024-03-31 21:37:29 +0300

    Коммит3 на f1 ветке

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Коммит1 на main ветке");
         System.out.println("Коммит2 на main ветке");
+        
+        System.out.println("Коммит3 на f1 ветке");
     }
 }
